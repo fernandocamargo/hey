1574431197e5af0a9d839a4bb96f8733325c08b4
Fernando Camargo Del Buono <camargodelbuono@gmail.com>
2019-10-30 11:51:22 +0100

    Hide back to results link

--- src/components/templates/pages/job/index.js
+++ src/components/templates/pages/job/index.js
@@ -8,15 +8,13 @@ import Details from './details';
 import { selectById } from './selectors';
 import withStyle from './style';
 
-const JobPage = ({ pageContext: { id }, className, location }) => {
+const JobPage = ({ pageContext: { id }, className }) => {
   const job = useSelector(selectById(id));
   const { title, description } = job;
 
   return (
     <Page title={title} description={description}>
       <section className={className}>
-        <h1>Go back to the results</h1>
-        <pre>{JSON.stringify(location, null, 2)}</pre>
         <Job {...job}>{Details}</Job>
       </section>
     </Page>
